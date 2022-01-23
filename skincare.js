4c5cf53a90b6d6af538bf39cf47c134d22abeffb
Mohak-Tripathi <93336602+Mohak-Tripathi@users.noreply.github.com>
2022-01-23 12:16:05 -0500

    Add files via upload

---
+++ skincare.js
@@ -0,0 +1,472 @@
+
+
+
+    userdata = [ 
+    {
+      
+      topname: "Top Rated",
+      image: " https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E1LM01_600x600_0.jpg",
+      product_name: "VITAMIN ENRICHED FACE BASE",
+      subname: "Multitasking, moisturizing primer",
+      price: 64.00,
+      cat: "moist",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_E1LM_80x80_0.jpg",
+
+    },
+    {
+      
+      topname: "New",
+      image: " https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EL1901_600x600_1.jpg",
+      product_name: "VITAMIN ENRICHED EYE BASE",
+      subname: "Undereye Moisturizer and Primer",
+      price: 56.00,
+      cat: "moist",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EL1901_80x80_0.jpg",
+    },
+    {
+      
+      topname: "New",
+      image: " https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EREP01_600x600_1.jpg",
+      product_name: "EXTRA REPAIR MOISTURE CREAM",
+      subname: "Replenishing, peptide-infused cream",
+      price: 105.00,
+      cat: "clean",
+      cartimg:" https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EREP01_80x80_0.jpg",
+
+    },
+    {
+      
+      topname: "New",
+      image: " https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EP7P01_600x600_1.jpg",
+      product_name: "EXTRA REPAIR EYE CREAM INTENSE",
+      subname: "Revitalizing undereye treatment",
+      price: 82.00,
+      cat: "eye",
+      cartimg:" https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EP7P01_80x80_0.jpg",
+    },
+    {
+      
+      topname: "New",
+      image: " https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EL1901_600x600_1.jpg",
+      product_name: "EXTRA CLEANSING BALM",
+      subname: "Purifying, conditioning cleanser",
+      price: 56.00,
+      cat: "clean",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EP7P01_80x80_0.jpg",
+    },
+    {
+      
+      topname: "Best Seller",
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EPHC01_600x600_1.jpg",
+      product_name: "EXTRA REPAIR EYE CREAM INTENSE REFILL",
+      subname: "Skin loving. Planet conscious",
+      price: 68.00 ,
+      cat: "eye",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_ERAN01_80x80_0.jpg"
+    },
+    {
+      
+      topname: "Best Rated",
+      image: " https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_ERER01_600x600_1.jpg",
+      product_name: "EXTRA REPAIR MOISTURE CREAM INTENSE REFILL",
+      subname: "Refill. Reuse. Repeat",
+      price: 85.00,
+      cat: "clean",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EPHC01_80x80_0.jpg" ,
+    },
+    {
+      
+      topname: "Best Rated",
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E65X01_600x600_0.jpg",
+      product_name: "HYDRATING FACE CREAM",
+      subname: "Lightweight Face Moisturizer",
+      price: 64.00,
+      cat: "clean",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_ERER01_80x80_0.jpg",
+
+    },
+    {
+      
+      topname: "Customer Favourite",
+      image: " https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E65Y01_600x600_0.jpg",
+      product_name: "HYDRATING EYE CREAM",
+      subname: "Gentle, quick-absorbing cream",
+      price: 56.00,
+      cat: "eye",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_E65Y_80x80_0.jpg",
+    },
+
+    {
+      
+      topname: "Best Rated",
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EMNE01_600x600_1.jpg",
+      product_name: "HYDRATING WATER FRESH CREAM",
+      subname: "100 hours of non-stop hydration",
+      price: 64.00,
+      cat: "clean",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EMNE01_80x80_0.jpg",
+    },
+
+    {
+      
+      topname: "",
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_ET7G01_600x600_1.jpg",
+      product_name: "HYDRATING EYE CREAM",
+      subname: "Gentle, quick-absorbing cream",
+      price: 56.00,
+      cat: "eye",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_E65Y_80x80_0.jpg",
+    },
+    {
+      
+      topname: "New",
+      image: " https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E65Y01_600x600_0.jpg",
+      product_name: "DELUXE SIZE REMEDIES SKIN CLARIFIER",
+      subname: "Pore-clarifying booster",
+      price: 74.00,
+      cat: "serum",
+      cartimg:"https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EFX201_80x80_0.jpg",
+    },
+    { 
+      topname: "New",
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E1YJ01_600x600_0.jpg",
+      product_name: "EXTRA FACE OIL",
+      subname: "Conditioning-blend face oil",
+      price: 72.00,
+      cat: "serum",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EFX201_80x80_0.jpg",
+    },
+    {
+      
+      topname: "New",
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EHP201_600x600_1.jpg",
+      product_name: "EXTRA ILLUMINATING MOISTURE BALM",
+      subname: "Luminizing pearl moisturizer",
+      price: 69.00,
+      cat: "moist",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_sku_EHP201_80x80_0.jpg",
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EER901_600x600_1.jpg",
+      product_name: "EXTRA LIP TINT",
+      subname: "Color-Enhancing Lip Balm",
+      price: 34.00,
+      cat: "moist",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_sku_EER901_80x80_0.jpg",
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EM4201_600x600_0.jpg",
+      product_name: "SOOTHING CLEANSING OIL",
+      subname: "Makeup Remover and Face Cleanser",
+      price: 50.00,
+      cat: "eye",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EM4201_80x80_0.jpg", 
+    },
+    {
+      
+      topname: "New",
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_ETCT01_600x600_0.jpg",
+      product_name: "SOOTHING CLEANSING OIL",
+      subname: "Makeup Remover and Face Cleanser",
+      price: 50.00,
+      cat: "eye",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EFX201_80x80_0.jpg",
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E6M301_600x600_0.jpg",
+      product_name: "EXTRA REPAIR SERUM",
+      subname: "Firming and moisturizing formula",
+      price: 119.00,
+      cat: "serum",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_E6M3_80x80_0.jpg", 
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EFX201_600x600_1.jpg",
+      product_name: "SKIN CLARIFIER NO. 75",
+      subname: "Pore-Minimizing Face Serum",
+      price: 49.00,
+      cat: "serum",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EFX201_80x80_0.jpg", 
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EG7801_600x600_1.jpg",
+      product_name: "SKIN MOISTURE SOLUTION NO. 86 - INTENSE REHYDRATION COMPOUND",
+      subname: "Water-infused hydrating serum",
+      price: 49.00,
+      cat: "serum",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EG7801_80x80_0.jpg" ,
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E6M301_600x600_0.jpg",
+      product_name: "EXTRA REPAIR SERUM",
+      subname: "Firming and moisturizing formula",
+      price: 119.00,
+      cat: "serum",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_E6M3_80x80_0.jpg", 
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E65R01_600x600_0.jpg",
+      product_name: "HYDRATING FACE TONIC",
+      subname: "Balancing and restoring toner",
+      price: 38.00,
+      cat: "clean",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_E65R_80x80_0.jpg",
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EPGP01_600x600_1.jpg",
+      product_name: "MINI SOOTHING CLEANSING OIL",
+      subname: "On-the-go gentle makeup-dissolving oil",
+      price: 15.00,
+      cat: "eye",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_sku_EPGP01_80x80_0.jpg",
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EPGN01_600x600_1.jpg",
+       product_name: "MINI VITAMIN ENRICHED FACE BASE",
+      subname: "Oil-free moisturizer and primer",
+      price: 20.00,
+      cat: "moist",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_sku_EPGP01_80x80_0.jpg",
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EKLT01_600x600_0.jpg",
+      product_name: "PRIMER PLUS PROTECTION SPF 50",
+      subname: "Protective face primer",
+      price: 40.00,
+      cat: "clean",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EKLT01_80x80_0.jpg", 
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EPGN01_600x600_1.jpg",
+      product_name: "MINI VITAMIN ENRICHED FACE BASE",
+      subname: "Oil-free moisturizer and primer",
+      price: 20.00,
+      cat: "moist",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_E65R_80x80_0.jpg",
+    },
+    {
+      
+      topname: "Best Seller",
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_E42G01_600x600_0.jpg",
+      product_name: "INSTANT LONG-WEAR MAKEUP REMOVER",
+      subname: "Extra-gentle eye makeup remover",
+      price: 33.00,
+      cat: "eye",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_E65R_80x80_0.jpg",
+    },
+    {
+      
+      
+      image: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_ELN701_600x600_0.jpg",
+      product_name: "DELUXE SIZE VITAMIN ENRICHED FACE BASE",
+      subname: "Bestselling priming moisturizer",
+      price: 95.00,
+      cat: "moist",
+      cartimg: "https://m.bobbibrowncosmetics.com/media/export/cms/products/80x80/bb_prod_EMNE01_80x80_0.jpg", 
+    }
+
+    ]
+
+
+   
+    cartarr= JSON.parse(localStorage.getItem("cartdata")) || [];
+    
+    display(userdata);
+
+    var data1= userdata;
+   
+    
+
+
+    document.querySelector(".totalproducts").textContent=`${userdata.length} products`
+
+
+
+    function filterCart(){
+    var selected=document.querySelector("#filterlist").value;
+
+    if(selected=="All"){
+        
+        document.querySelector(".totalproducts").textContent=`${userdata.length} products`
+        display(userdata);
+        data1= userdata
+        document.querySelector("#sortPrice").value="";
+    }
+    else{
+    var res= userdata.filter(function (elem){
+        return elem.cat== selected;
+
+       
+    })
+
+    console.log(res);
+
+ 
+
+    document.querySelector(".totalproducts").textContent=`${res.length} products`
+    
+
+    if(selected=="moist"){
+    
+    display(res);
+    
+
+    data1=res
+    document.querySelector("#sortPrice").value="";
+
+}
+else if(selected=="serum"){
+   
+  display(res);
+  
+
+  data1=res
+    document.querySelector("#sortPrice").value="";
+}
+else if(selected=="clean"){
+  display(res);
+  
+
+  data1=res
+    document.querySelector("#sortPrice").value="";
+}
+else if(selected=="eye"){
+    
+  display(res);
+  
+  data1=res
+    document.querySelector("#sortPrice").value="";
+}
+}
+}
+
+      
+    
+function sortPrice(){
+  var selected= document.querySelector("#sortPrice").value;
+
+  var res=data1.sort(function(a,b){
+        if(selected=="Low"){
+            return a.price-b.price;
+        }
+        else if (selected=="High"){
+           return b.price-a.price;
+        }
+    })
+   
+
+    // document.querySelector("#container").innerHTML="";
+    display(res);
+}
+
+ 
+
+
+function display(userdata){
+  document.querySelector("#container").innerHTML="";
+    userdata.map(function (elem) {
+
+
+
+        var div= document.createElement("div");
+
+        var topname= document.createElement("p");
+        topname.textContent= elem.topname;
+        topname.setAttribute("class", "topname")
+
+      
+
+        var image = document.createElement("img");
+    image.setAttribute("src", elem.image);
+    image.setAttribute("class", "imageclassic")
+
+
+
+    var name= document.createElement("h3");
+    name.textContent= elem.product_name;
+    name.setAttribute("class", "nameclassic")
+
+    var subname = document.createElement("p");
+    subname.textContent= elem.subname;
+
+    subname.setAttribute("class", "subnameclassic")
+
+
+    
+    var div1 = document.createElement("div");
+
+    var symbol= document.createElement("i")
+    symbol.setAttribute("class", "fas fa-star")
+
+    var symbol1= document.createElement("i")
+    symbol1.setAttribute("class", "fas fa-star")
+
+    var symbol2= document.createElement("i")
+    symbol2.setAttribute("class", "fas fa-star")
+
+    var symbol3= document.createElement("i")
+    symbol3.setAttribute("class", "fas fa-star-half-alt")
+    
+
+    div1.append(symbol, symbol1, symbol2, symbol3)
+
+    var price= document.createElement("p");
+    price.textContent= `$${elem.price}`
+    price.setAttribute("class", "priceclassic")
+
+    // var div1= document.createElement("div");
+
+    var addcart= document.createElement("button");
+    addcart.textContent= "ADD TO BAG";
+    addcart.setAttribute("class", "addcartclassic")
+    addcart.addEventListener("click", addTocart) 
+      
+    
+
+    // div1.append(addcart)
+      if(topname.textContent != "")
+{
+        div.append(topname, image, name, subname, div1, price, addcart);
+      }
+      else{
+        div.append(image, name, subname, div1, price, addcart);
+      }
+
+   
+
+    document.querySelector("#container").append(div);
+
+
+
+    })
+
+    function addTocart(elem){
+console.log(elem);
+cartarr.push(elem);
+localStorage.setItem("cartdata", JSON.stringify(cartarr));
+alert("added to cart successfully")
+}
+
+  }
