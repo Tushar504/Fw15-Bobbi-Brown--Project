bb73b2ec659a5fcda4e32a31ea6303fbbf3b5aa0
Nishut Suman Mahunta <67069496+NishutSuman@users.noreply.github.com>
2022-01-23 21:00:43 +0530

    Data File Uploaded

---
+++ bb_data.js
@@ -0,0 +1,210 @@
+var seasonal_large_pic= "	https://www.bobbibrowncosmetics.com/media/export/cms/HP_Modules/SS22/SS22_StayAllDayLips_pc.jpg"
+
+var seasonalData= [
+    {images: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EHJA10_600x600_0.jpg",
+     clr:"BOSS PINK",
+     p_name:"LUXE MATTE LLIP COLOR",
+     p_tag:"Intense, ultra-matte lipstick",
+     price:"$38.00"
+    },
+    {images: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EHJA10_600x600_0.jpg",
+     clr:"BITTEN PEACH",
+     p_name:"LUXE MATTE LLIP COLOR",
+     p_tag:"Intense, ultra-matte lipstick",
+     price:"$38.00"
+    },
+    {images: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EHJA10_600x600_0.jpg",
+     clr:"RED CARPET",
+     p_name:"LUXE MATTE LLIP COLOR",
+     p_tag:"Intense, ultra-matte lipstick",
+     price:"$38.00"
+    },
+    {images: "https://www.bobbibrowncosmetics.com/media/export/cms/products/600x600/bb_sku_EHJA09_600x600_0.jpg",
+     clr:"BOSS PINK",
+     p_name:"LUXE MATTE LLIP COLOR",
+     p_tag:"Intense, ultra-matte lipstick",
+     price:"$38.00"
+    }
+
+
+]
+
+var newYearExtraCare= [
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/cms/HP_Modules/SS22/SS22_WinterSkincare_pc_1.jpg",
+        names:"EXTRA CLEANSING BALM",
+        cmnt:"\"This is a beautiful face wash- really balmy, hydrating and super luxorious.",
+        artist:"-Cassandra Garcia, Global Pro Artist\""
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/cms/HP_Modules/SS22/SS22_WinterSkincare_pc_2.jpg",
+        names:"EXTRA REPAIR MOISTURE CREAM INTENSE",
+        cmnt:"\"This repairing moisturizer penetrates the skin seamlessly and crates the prfect cushioned base for makeup.\"",
+        artist:"-Cassandra Garcia, Global Pro Artist"
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/cms/HP_Modules/SS22/SS22_WinterSkincare_pc_3.jpg",
+        names:"EXTRA REPAIR EYE CREAM INTENSE",
+        cmnt:"\"This rich eye cream cushions and plumps fine lines. It's super reparative.\"",
+        artist:"-Cassandra Garcia, Global Pro Artist"
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/cms/HP_Modules/SS22/SS22_WinterSkincare_pc_4.jpg",
+        names:"EXTRA FACE OIL",
+        cmnt:"\"This is one of my favourite products. It mimics the natural oils in the skin, making it a great boost in the winter time.\"",
+        artist:"-Cassandra Garcia, Global Pro Artist"
+    }
+]
+
+var lastAboveFooter=[
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/c…EvergreenServices/BB_HP_Module_PC_Services_03.jpg",
+        names:"CHAT NOW",
+        details:"Chat with a makeup artist to get product reccomendations",
+        btn:"CHAT NOW"
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/c…EvergreenServices/BB_HP_Module_PC_Services_05.jpg",
+        names:"FREE SHIPPING",
+        details:"Get free shipping on all orders $65+. Or join Bobbi Brown Club to get free shipping on all orders.",
+        btn:"LEARN MORE"
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/c…EvergreenServices/BB_HP_Module_PC_Services_07.jpg",
+        names:"WE WANT YOU TO LOVE IT",
+        details:"That's why we offer free shipping and returns on all foundations, correctors, concealers, and powders.",
+        btn:"LEARN MORE"
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/c…EvergreenServices/BB_HP_Module_PC_Services_09.jpg",
+        names:"EXCLUSIVE OFFERS",
+        details:"Special somethings, just for you.",
+        btn:"GET OFFERS"
+    }
+];
+
+
+var learnFromThePros= [
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/c…VirtualServices/SS21_customizeskincareroutine.jpg",
+        time: "30 MIN",
+        names:"CUSTOMIZE YOUR SKINCARE ROUTINE",
+        details:"Learn how to address your top skin concerns with a personalized routine curated by an artist."
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/c…VirtualServices_LandingPage_Group_Tile2_1_@2x.gif",
+        time: "15 MIN",
+        names:"LOOK LESS TIRED",
+        details:"Learn how to neutralize under eye circles and look instantly more awake and bright-eyed using a few easy essentials."
+    },
+    {
+        images:"	https://www.bobbibrowncosmetics.com/media/export/cms/VirtualServices/SS21_lookfreshvideocalls.jpg",
+        time: "30 MIN",
+        names:"LOOK FRESH FOR VIDEO CALLS",
+        details:"Learn how to look bright eyed and fresh faced in minutes with our pro tips."
+    },
+    {
+        images:"	https://www.bobbibrowncosmetics.com/media/export/cms/VirtualServices/SS21_masteryourbrows.jpg",
+        time: "30 MIN",
+        names:"MASTER YOUR BROWS",
+        details:"Learn how to feel, define, and enhance your natural shape for an effortlessly polished look."
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/c…ltations_Consultation_Tile_Weekend_Glow_1_@2x.jpg",
+        time: "30 MIN",
+        names:"EASY GLOWING",
+        details:"In this session, you'll learn how to how to effortlessly boost skin's radiance with a few glow essentials."
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/cms/VirtualServices/SS21_effortlesseyes.jpg",
+        time: "30 MIN",
+        names:"EFFORTLESSLY EYES",
+        details:"Learn how to choose the right shades, finishes and techniques to enhance your eye shape and skin tone using Long-Wear Cream Shadow Sticks."
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/cms/VirtualServices/SS21_fiveminutemakeup.jpg",
+        time: "30 MIN",
+        names:"5-MINUTE MAKEUP",
+        details:"Learn how to get ready in no time using five essentials and a personalized game plan by one of our artists."
+    },
+    {
+        images:"https://www.bobbibrowncosmetics.com/media/export/cms/VirtualServices/SS21_findyourfoundation.jpg",
+        time: "30 MIN",
+        names:"FIND YOUR FOUNDATION",
+        details:"Discover your feel-good coverage, weightless formula, and ideal finish in the complementary artist consultation."
+    },
+    {
+        images:"	https://www.bobbibrowncosmetics.com/media/export/cms/VirtualServices/SS21_makeupyourway.jpg",
+        time: "15 MIN",
+        names:"MAKEUP YOUR WAY",
+        details:"In this customizable session, an artist will help you create your best look from boosting your glow to mastering your brows to amplifying eyes."
+    }
+];
+
+var bbCommunity= [
+    {
+        bg: "https://z3photorankmedia-a.akamaihd.net/media/x/m/x/xmxbiv4/mobile.jpg",
+        tag: "@BOBBIBROWNAUSTRALIA"
+    },
+    {
+        bg: "https://z1photorankmedia-a.akamaihd.net/media/t/t/k/ttkqsq4/mobile.jpg",
+        tag: "@RANTSMUA"
+    },
+    {
+        bg: "https://z2photorankmedia-a.akamaihd.net/media/2/q/e/2qef9q4/mobile.jpg",
+        tag: "@BB_JP_SAKUTA"
+    },
+    {
+        bg: "https://z2photorankmedia-a.akamaihd.net/media/m/a/s/masg5a4/mobile.jpg",
+        tag: "@BBPRO_AIMEEMORRISON"
+    },
+    {
+        bg: "https://photorankmedia-a.akamaihd.net/media/n/r/p/nrpnb94/mobile.jpg",
+        tag: "@BBPRO_TH_PICHAPHAT"
+    },
+    {
+        bg: "https://z2photorankmedia-a.akamaihd.net/media/6/w/p/6wp3mq4/mobile.jpg",
+        tag: "@BBPRO_JP_SAKI"
+    },
+    {
+        bg: "https://z2photorankmedia-a.akamaihd.net/media/s/a/9/sa9kj94/mobile.jpg",
+        tag: "@BAEDYXO"
+    },
+    {
+        bg: "https://photorankmedia-a.akamaihd.net/media/2/9/h/29hmam4/mobile.jpg",
+        tag: "@NIKKIDEROEST"
+    },
+    {
+        bg: "https://z2photorankmedia-a.akamaihd.net/media/6/x/u/6xumap4/mobile.jpg",
+        tag: "@BBAKROPOLERIGA"
+    },
+    {
+        bg: "https://z1photorankmedia-a.akamaihd.net/media/9/m/3/9m39xq4/mobile.jpg",
+        tag: "@BOBBIBROWNKOREA"
+    },
+    {
+        bg: "https://z3photorankmedia-a.akamaihd.net/media/6/7/3/67336k4/mobile.jpg",
+        tag: "@BBPRO_WARRENDOWDALL"
+    },
+    {
+        bg: "https://photorankmedia-a.akamaihd.net/media/j/3/o/j3onnq4/mobile.jpg",
+        tag: "@BOBBIBROWNTURKIYE"
+    },
+    {
+        bg: "https://z3photorankmedia-a.akamaihd.net/media/9/x/8/9x8p5y4/mobile.jpg",
+        tag: "@ITSME_TESS02"
+    },
+    {
+        bg: "https://z3photorankmedia-a.akamaihd.net/media/j/4/g/j4ghnt4/mobile.jpg",
+        tag: "@AGNESTAN10061997"
+    }
+]
+
+
+var a=0
+var b=1
+while(a<20){
+    b--;
+    a++;
+}
+console.log(a,b)
